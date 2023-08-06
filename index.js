e423b885c2d7721a2867f31553beda189edbc3ad
Krystle Lee <l33.krys@gmail.com>
2023-08-05 17:33:19 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(arr) {
+    let answer = arr.find(element => element.result === "W")
+    if (answer) {
+      return answer.year;
+    } else {
+      undefined;
+    }
+  }
+
